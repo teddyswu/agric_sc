90468389b5b6a69dd929a7e639328bda4f07d222
wu junlin <welcometeddy@yahoo.com.tw>
2018-05-25 15:21:15 +0800

    匯出工作記錄

--- app/controllers/work_records_controller.rb
+++ app/controllers/work_records_controller.rb
@@ -4,20 +4,21 @@ class WorkRecordsController < ApplicationController
 	end
 
 	def outputs
-		up = UserProfile.find_by_fb_uid(params[:uid])
+		up = UserProfile.find_by_name(params[:name])
 		beginning = "#{params[:s_month]}".to_i
 		@work_projects = "" 
 		WorkProject.where(:record_type =>params[:record_type]).each do |wp|
       @work_projects << wp.name
       @work_projects << "<br >"
     end
-		@records = WorkRecord.where(:record_type => params[:record_type], :owner_id => up.user_id, :created_at => beginning.month.ago .. Time.now).paginate(:page => params[:page], per_page: 10)
+		@records = WorkRecord.where(:record_type => params[:record_type], :owner_id => up.user_id, :created_at => beginning.month.ago .. Time.now)
 		respond_to do |format|
     	format.html
 	    format.pdf do
 	    	pdf = RecordPdf.new(@records, @work_projects, params[:record_type])
 	    	send_data pdf.render, type: "application/pdf",
-	    												disposition: "inline"
+	    												disposition: "inline",
+	    												filename: "Record_#{params[:name]}_#{params[:s_month]}"
 	    end
 	  end
 	end
