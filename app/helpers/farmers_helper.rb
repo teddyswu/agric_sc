230624fb27afdf2e8c3043d72d503e4a18a8465f
wu junlin <welcometeddy@yahoo.com.tw>
2018-07-26 17:30:15 +0800

    加上影片判斷

--- app/helpers/farmers_helper.rb
+++ app/helpers/farmers_helper.rb
@@ -1,2 +1,19 @@
 module FarmersHelper
+	def render_tag(url)
+		if url.include?("jpg") 
+			return image_tag url , class: "img-fluid d-block"
+		else
+			return "<video id=\"movie\" preload controls loop width=\"536\">
+        <source src=\"#{url}\" type=\"video/mp4\" />
+      </video>".html_safe
+		end
+	end
+
+	def render_first_image(url)
+		if url.include?("jpg")
+			return image_tag url, class: "img-fluid text-center"
+		else
+			return image_tag "https://i.imgur.com/BQnEqUW.png", class: "img-fluid text-center"
+		end
+	end
 end
