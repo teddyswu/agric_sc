e715d5be5e5d13801f416b4736bb850aad7efc8f
wu junlin <welcometeddy@yahoo.com.tw>
2018-09-04 13:01:00 +0800

    串接修正

--- app/uploaders/fb_to_aws_uploader.rb
+++ app/uploaders/fb_to_aws_uploader.rb
@@ -96,7 +96,7 @@ class FbToAwsUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png mp4)
   end
 
   # Override the filename of the uploaded files:
