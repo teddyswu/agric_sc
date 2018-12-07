710933d285bac972c8f2a649eb452aa9c6792840
wu junlin <welcometeddy@yahoo.com.tw>
2018-12-07 16:21:58 +0800

    domain調整

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 custom = YAML.load_file("config/customization.yml")
-Rails.application.config.session_store :cookie_store, key: '_ugooz_session', domain: :custom["domain"]
+Rails.application.config.session_store :cookie_store, key: '_ugooz_session', domain: custom[:domain]
