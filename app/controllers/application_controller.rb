0dcaca8f234855304696fb83b6a6380398dcdcf2
wu junlin <welcometeddy@yahoo.com.tw>
2018-12-14 17:38:52 +0800

    更新http轉址

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
-  before_action :deny_ip
+  before_action :deny_ip,
+                :http_check
 
   def after_sign_in_path_for(resource_or_scope)
   	current_user.is_admin == true ? agris_path : root_path
@@ -18,5 +19,11 @@ class ApplicationController < ActionController::Base
   def deny_ip
     deny_ip = ["41.44.109.146", "72.89.36.208","103.228.110.123"]
     render_404 if deny_ip.include?(request.remote_ip)
+  end
+
+  def http_check
+    if request.host.include?("ugooz")
+      redirect_to request.original_url.gsub("http","https"), :status => 301 if request.original_url.include?("http://")
+    end
   end
 end
