03070bc8fff6ca04ac352edbe5ba5dbf7ba1f11b
wu junlin <welcometeddy@yahoo.com.tw>
2019-01-07 12:14:55 +0800

    修正

--- app/controllers/work_walls_controller.rb
+++ app/controllers/work_walls_controller.rb
@@ -1,9 +1,9 @@
 class WorkWallsController < ApplicationController
 	def index
-		@big_head = WorkDiaryImage.where(:position => 1).order("updated_at DESC").limit(1)
-		@small_rectangle = WorkDiaryImage.where(:position => 2).order("updated_at DESC").limit(8)
-		@straight_bar = WorkDiaryImage.where(:position => 3).order("updated_at DESC").limit(3)
-		@bar = WorkDiaryImage.where(:position => 4).order("updated_at DESC").limit(1)
+		@big_head = WorkDiaryImage.where(:position => 1, :enabled => true).order("updated_at DESC").limit(1)
+		@small_rectangle = WorkDiaryImage.where(:position => 2, :enabled => true).order("updated_at DESC").limit(8)
+		@straight_bar = WorkDiaryImage.where(:position => 3, :enabled => true).order("updated_at DESC").limit(3)
+		@bar = WorkDiaryImage.where(:position => 4, :enabled => true).order("updated_at DESC").limit(1)
 		render layout: "story"
 	end
 end
