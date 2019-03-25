16967623d70b64b2c698b3a0c06fab5cf2ebf3f7
wu junlin <welcometeddy@yahoo.com.tw>
2019-03-25 19:46:43 +0800

    修正

--- app/controllers/personal_interplays_controller.rb
+++ app/controllers/personal_interplays_controller.rb
@@ -11,12 +11,30 @@ class PersonalInterplaysController < ApplicationController
   end
 
   def edit
+    @personal_inter = PersonalInterplay.find(params[:id])
   end
 
   def create
+    @personal_inter = PersonalInterplay.new(personal_interplay_params)
+    @personal_inter.save!
+    redirect_to :action => :index
   end
 
   def update
+    @personal_inter = PersonalInterplay.find(params[:id])
+    @personal_inter.update(personal_interplay_params)
+    redirect_to :action => :index
+  end
+
+  def destroy
+    @personal_inter = PersonalInterplay.find(params[:id])
+    @personal_inter.destroy
+
+    redirect_to :action => :index
+  end
+
+  def personal_interplay_params
+    params.require(:personal_interplay).permit(:qr_name, :start_model)
   end
 
 
