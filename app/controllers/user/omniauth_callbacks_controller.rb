c951e4a8cd5d9035131a6d8996c92bb3f41da7c3
wu junlin <welcometeddy@yahoo.com.tw>
2019-05-09 13:02:42 +0800

    恢復連結

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -103,22 +103,22 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       end
       total << card
     end
-    # customization = YAML.load_file("config/customization.yml")
-    # uri = URI.parse(customization[:user_message_post])
-    # user = customization[:user]
-    # password = customization[:password]
-    # post_data = {'recipient_id'=> scoped_id, 'user' => user, 'password' => password, 'elements' => total }.to_json
-    # https = Net::HTTP.new(uri.host,uri.port)
-    # https.use_ssl = true
-    # req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-    # req.body = post_data
-    # res = https.request(req)
-    # File.open("#{Rails.root}/log/mm.log", "a+") do |file|
-    #   file.syswrite(%(#{Time.now.iso8601}: #{scoped_id} \n---------------------------------------------\n\n))
-    #   file.syswrite(%(#{Time.now.iso8601}: #{uri} \n---------------------------------------------\n\n))
-    #   file.syswrite(%(#{Time.now.iso8601}: #{post_data} \n---------------------------------------------\n\n))
-    #   file.syswrite(%(#{Time.now.iso8601}: #{res.body} \n---------------------------------------------\n\n))
-    # end
+    customization = YAML.load_file("config/customization.yml")
+    uri = URI.parse(customization[:user_message_post])
+    user = customization[:user]
+    password = customization[:password]
+    post_data = {'recipient_id'=> scoped_id, 'user' => user, 'password' => password, 'elements' => total }.to_json
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+    req.body = post_data
+    res = https.request(req)
+    File.open("#{Rails.root}/log/mm.log", "a+") do |file|
+      file.syswrite(%(#{Time.now.iso8601}: #{scoped_id} \n---------------------------------------------\n\n))
+      file.syswrite(%(#{Time.now.iso8601}: #{uri} \n---------------------------------------------\n\n))
+      file.syswrite(%(#{Time.now.iso8601}: #{post_data} \n---------------------------------------------\n\n))
+      file.syswrite(%(#{Time.now.iso8601}: #{res.body} \n---------------------------------------------\n\n))
+    end
   end
 
 
