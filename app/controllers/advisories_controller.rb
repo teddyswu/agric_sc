ce97ff2eeafcb06ea084c5ab9ecef6af850fd2a0
wu junlin <welcometeddy@yahoo.com.tw>
2019-09-04 10:34:10 +0800

    調整

--- app/controllers/advisories_controller.rb
+++ app/controllers/advisories_controller.rb
@@ -14,6 +14,7 @@ class AdvisoriesController < ApplicationController
     when 18..23
       sta = "晚餐"
     end
+    cons = Consultation.find_by(:id => pl[1])
     con_option = ConsultationOption.find(pl[2])
     auth = Authorization.find_by_uid(params[:uid])
     auth.user.user_profile.age_range > 4 ? current_age = 4 : current_age = 3
@@ -38,7 +39,31 @@ class AdvisoriesController < ApplicationController
     text_3["text"] = con_content[0].content
     text_3["delay"] = "1"
     text_t << text_3
+    text_4 = Hash.new
+    text_4["Name"] = "ugooz.b2c.adviser.01.04"
+    text_4["type"] = "text"
+    text_4["text"] = "查看其他餐點"
+    text_4["delay"] = "1"
+    text_t << text_4
     total_text << text_t
+    card_t = Array.new
+    cons.consultation_cates.limit(9).order(:sort).each_with_index do |ccat, i|
+      card_s = Hash.new
+      card_s["NAME"] = "ugooz.b2c.adviser.#{cons.id}.0#{total_text[0].size+1}.0#{i+1}"
+      card_s["title"] = ccat.name
+      card_s["image_url"] = ccat.pic
+      button_t = Array.new
+      ccat.consultation_options.limit(3).order(:sort).each do |copt|
+        button_si = Hash.new
+        button_si["type"] = "postback"
+        button_si["title"] = copt.name
+        button_si["payload"] = "u_#{cons.id}_#{copt.id}"
+        button_t << button_si
+      end
+      card_s["buttons"] = button_t
+      card_t << card_s
+    end
+    total_text << card_t
     render text: total_text
 	end
 end
